0c799067a3efe0975d3538a0fb1e5a479da93778
Bilail <bilail.benyahia@outlook.fr>
2022-03-25 17:34:31 +0100

    client.java completed

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,13 +1,18 @@
-import fr.polytech.grpc.simple.OpGrpc;
-import fr.polytech.grpc.simple.OpGrpc.*;
-import fr.polytech.grpc.simple.Service;
+import fr.polytech.grpc.proto.Service.*;
+import fr.polytech.grpc.proto.ProtoGrpc;
+import fr.polytech.grpc.proto.Service.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Client {
 
     private ManagedChannel channel;
-    protected OpBlockingStub blockingStub;
+    protected ProtoGrpc.ProtoBlockingStub blockingStub;
 
     public Client(String host, int port) {
         channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
@@ -15,22 +20,29 @@ public class Client {
     }
 
     protected void initStubs(ManagedChannel channel) {
-        blockingStub = OpGrpc.newBlockingStub(channel);
+
+        blockingStub = ProtoGrpc.newBlockingStub(channel);
     }
 
-    public long calc(int int1, int int2, Service.Operation operation) throws IllegalArgumentException{
-        if (operation == Service.Operation.DIV && int2 == 0)
-            throw new IllegalArgumentException("Division By Zero Error!");
-        Service.Input request = Service.Input.newBuilder().setInt1(int1).setInt2(int2).setOperation(operation).build();
-        Service.Result reply = blockingStub.calculate(request);
-        return reply.getResult();
+    public Reply f(Info inf, Map<String, Data> data){
+         Request request = Request.newBuilder().setInfo(inf).putAllData(data).build();
+         Reply reply = blockingStub.handle(request);
+         return reply;
+
     }
 
     public static void main(String[] args) {
         Client client = new Client("localhost", 1664);
 
-        System.out.println(client.calc(1, 2, Service.Operation.MUL));
-        System.out.println(client.calc(1000, 5, Service.Operation.DIV));
+        Info i = Info.newBuilder().setSender("Dupont").setTimestamp(0101010).setId(01).build();
+
+        //Data d1 = Data.newBuilder().setData1((float)1.5).setData2(true).setData3(new ArrayList<int>(Arrays.asList(1,2,3)));
+        Data.Builder d1 = Data.newBuilder().setData1((float)1.5).setData2(true).addAllData3(new ArrayList<int>(Arrays.asList(1,2,3)));
+
+
+        Map<String, Data> d = (Map<String, Data>) new HashMap<>().put("test",d1);
+        System.out.println(client.f(i,d));
+
 
         System.out.println("Fin");
     }
