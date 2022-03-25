095d7ca8bf2ed2056c48f9ac110eadef1ff19ead
Bilail <bilail.benyahia@outlook.fr>
2022-03-25 18:38:20 +0100

    final

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -19,10 +19,15 @@ public class Client {
     }
 
     protected void initStubs(ManagedChannel channel) {
-
         blockingStub = ProtoGrpc.newBlockingStub(channel);
     }
 
+    /**
+     *
+     * @param inf les informations de la personne qui envoie les données
+     * @param data les données envoyées
+     * @return renvoie la réponse
+     */
     public Reply sendDatatoServer(Info inf, Map<String, Data> data){
          Request request = Request.newBuilder().setInfo(inf).putAllData(data).build();
          Reply reply = blockingStub.handle(request);
@@ -30,9 +35,15 @@ public class Client {
 
     }
 
+    /**
+     *  Fonction Main on test avec 2 cas, un cas avec des datas et un
+     *  cas avec data vide qui lève un message d'erreur
+     * @param args
+     */
     public static void main(String[] args) {
         Client client = new Client("localhost", 1664);
 
+        System.out.println(" ----------- Test 1  ----------- ");
         Info info = Info.newBuilder().setSender("Dupont").setTimestamp(10101010).setId(101).build();
 
         Data d1 = Data.newBuilder().setData1((float)1.5).setData2(true).addAllData3(new ArrayList<>(Arrays.asList(1, 2, 3))).build();
@@ -41,8 +52,12 @@ public class Client {
         Map<String, Data> dataMap = new HashMap<>();
         dataMap.put("test1", d1);
         dataMap.put("test2", d2);
-
         System.out.println(client.sendDatatoServer(info, dataMap));
+
+        System.out.println(" \n\n----------- Test 2  ----------- ");
+
+        Info info2 = Info.newBuilder().setSender("Benoit").setTimestamp(1575640).setId(103).build();
+        System.out.println(client.sendDatatoServer(info2, new HashMap<>()));
 
 
         System.out.println("Fin");
